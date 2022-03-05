d7ec1dd1ffb352cde8d947386e5a77b0819008e7
kamu <joaopaulogabarrao@gmail.com>
2022-03-05 20:21:39 -0300

    featured crud categories

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    return  CategoryResource::collection(Category::all());
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \App\Http\Requests\StoreCategoryRequest  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(StoreCategoryRequest $request)
+  {
+    $category = Category::create($request->validated());
+
+    return CategoryResource::make($category);
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Models\Category  $category
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Category $category)
+  {
+    return CategoryResource::make($category);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \App\Http\Requests\UpdateCategoryRequest  $request
+   * @param  \App\Models\Category  $category
+   * @return \Illuminate\Http\Response
+   */
+  public function update(UpdateCategoryRequest $request, Category $category)
+  {
+    $category->update($request->validated());
+    return CategoryResource::make($category);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Models\Category  $category
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Category $category)
+  {
+    $category->delete();
+    return response()->json([], 204);
+  }
+}
