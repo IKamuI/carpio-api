a21e9bcaa1189d8063bfab63dc5060d63f4fb42a
kamu <joaopaulogabarrao@gmail.com>
2022-03-06 19:21:24 -0300

    add OrderItem Factory

---
+++ database/factories/OrderItemFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\OrderItem>
+ */
+class OrderItemFactory extends Factory
+{
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition()
+  {
+    return [
+      'order_id' => Order::factory(),
+      'product_id' => Product::factory(),
+      'product_name' => $this->faker->city,
+      'price' => $this->faker->numerify
+    ];
+  }
+}
