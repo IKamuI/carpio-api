f9e09385f324b4f8c7a8f8920fe13f717879e5ad
kamu <joaopaulogabarrao@gmail.com>
2022-03-06 23:04:13 -0300

    add breeze api

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\OrderItemController;
-use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,28 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-  return $request->user();
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
 });
-
-Route::apiResource('order/items', OrderItemController::class);
-
-// Route::middleware(['middleware' => 'auth:sanctum'])->group(function () {
-//   Route::apiResources(
-//     [
-//       'categories' => CategoryController::class,
-//       'products' => ProductController::class,
-//       'orders' => OrderController::class
-//     ],
-//     ['except' => ['index', 'show']]
-//   );
-// });
-
-Route::apiResources(
-  [
-    'categories' => CategoryController::class,
-    'products' => ProductController::class,
-    'orders' => OrderController::class
-  ],
-  // ['only' => ['index', 'show']]
-);
