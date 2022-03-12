14c3a4ee1e2b573284314975e2aff8d9140ae715
Kamu <joaopaulogabarrao@gmail.com>
2022-03-12 02:48:42 -0300

    configurado auto fix php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreOrderRequest;
@@ -9,68 +8,68 @@ use App\Models\Order;
 
 class OrderController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function index()
-  {
-    $order = Order::with(['products', 'client'])->paginate(20);
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $order = Order::with(['products', 'client'])->paginate(20);
 
-    return OrderResource::collection($order);
-  }
+        return OrderResource::collection($order);
+    }
 
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \App\Http\Requests\StoreOrderRequest  $request
-   * @return \Illuminate\Http\Response
-   */
-  public function store(StoreOrderRequest $request)
-  {
-    $data = $request->validated();
-    $order = Order::create($data);
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreOrderRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreOrderRequest $request)
+    {
+        $data = $request->validated();
+        $order = Order::create($data);
 
-    return OrderResource::make($order);
-  }
+        return OrderResource::make($order);
+    }
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  \App\Models\Order  $order
-   * @return \Illuminate\Http\Response
-   */
-  public function show(Order $order)
-  {
-    return OrderResource::make($order->load(['products', 'client']));
-  }
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Order $order)
+    {
+        return OrderResource::make($order->load(['products', 'client']));
+    }
 
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \App\Http\Requests\UpdateOrderRequest  $request
-   * @param  \App\Models\Order  $order
-   * @return \Illuminate\Http\Response
-   */
-  public function update(UpdateOrderRequest $request, Order $order)
-  {
-    $data = $request->validated();
-    $order->update($data);
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateOrderRequest  $request
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateOrderRequest $request, Order $order)
+    {
+        $data = $request->validated();
+        $order->update($data);
 
-    return OrderResource::make($order);
-  }
+        return OrderResource::make($order);
+    }
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  \App\Models\Order  $order
-   * @return \Illuminate\Http\Response
-   */
-  public function destroy(Order $order)
-  {
-    $order->delete();
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Order $order)
+    {
+        $order->delete();
 
-    return response()->json([], 204);
-  }
+        return response()->json([], 204);
+    }
 }
--- app/Http/Controllers/OrderItemController.php
+++ app/Http/Controllers/OrderItemController.php
@@ -1,76 +1,74 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreOrderItemRequest;
 use App\Http\Requests\UpdateOrderItemRequest;
 use App\Http\Resources\OrderItemResource;
-use App\Http\Resources\OrderResource;
 use App\Models\OrderItem;
 
 class OrderItemController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function index()
-  {
-    $ordersItems = OrderItem::paginate(10);
-    return OrderItemResource::collection($ordersItems);
-  }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $ordersItems = OrderItem::paginate(10);
+        return OrderItemResource::collection($ordersItems);
+    }
 
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \App\Http\Requests\StoreOrderItemRequest  $request
-   * @return \Illuminate\Http\Response
-   */
-  public function store(StoreOrderItemRequest $request)
-  {
-    $data = $request->validated();
-    $orderItem = OrderItem::create($data);
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreOrderItemRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreOrderItemRequest $request)
+    {
+        $data = $request->validated();
+        $orderItem = OrderItem::create($data);
 
-    return OrderItemResource::make($orderItem);
-  }
+        return OrderItemResource::make($orderItem);
+    }
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  \App\Models\OrderItem  $orderItem
-   * @return \Illuminate\Http\Response
-   */
-  public function show(OrderItem $item)
-  {
-    return OrderItemResource::make($item);
-  }
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OrderItem  $orderItem
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OrderItem $item)
+    {
+        return OrderItemResource::make($item);
+    }
 
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \App\Http\Requests\UpdateOrderItemRequest  $request
-   * @param  \App\Models\OrderItem  $orderItem
-   * @return \Illuminate\Http\Response
-   */
-  public function update(UpdateOrderItemRequest $request, OrderItem $item)
-  {
-    $data = $request->validated();
-    $item->update($data);
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateOrderItemRequest  $request
+     * @param  \App\Models\OrderItem  $orderItem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateOrderItemRequest $request, OrderItem $item)
+    {
+        $data = $request->validated();
+        $item->update($data);
 
-    return OrderItemResource::make($item);
-  }
+        return OrderItemResource::make($item);
+    }
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  \App\Models\OrderItem  $orderItem
-   * @return \Illuminate\Http\Response
-   */
-  public function destroy(OrderItem $item)
-  {
-    $item->delete();
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OrderItem  $orderItem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OrderItem $item)
+    {
+        $item->delete();
 
-    return response()->json([], 204);
-  }
+        return response()->json([], 204);
+    }
 }
